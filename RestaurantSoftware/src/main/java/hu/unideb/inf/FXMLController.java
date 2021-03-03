cd9511f696fb1b67315c2919a50621dcc19ac049
zolee09 <zolee09@citromail.hu>
2021-03-03 12:07:27 +0100

    Signed-off-by: zolee09 <zolee09@citromail.hu>

--- RestaurantSoftware/src/main/java/hu/unideb/inf/FXMLController.java
+++ RestaurantSoftware/src/main/java/hu/unideb/inf/FXMLController.java
@@ -1,18 +1,21 @@
 package hu.unideb.inf;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class FXMLController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private AnchorPane rootPane;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -22,4 +25,10 @@ public class FXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void loadOrder(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/fxml/Scene2.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
 }
